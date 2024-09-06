a65f02abb9e138af03c8748e2d58f1a07002babd
Nisha Chavan <nisha.chavan@lekab.com>
2024-09-06 19:17:58 +0200

    import MonacoEditor from 'react-monaco-editor';

--- frontend/components/CodeEditor.tsx
+++ frontend/components/CodeEditor.tsx
@@ -1,31 +1,24 @@
-import {useState} from "react";
-import {VerticalLayout} from "@hilla/react-components/VerticalLayout";
+import MonacoEditor from 'react-monaco-editor';
 
 const CodeEditor = () => {
-    const [code, setCode] = useState('');
+    const options = {
+        selectOnLineNumbers: true,
+        automaticLayout: true,
+        // Disable telemetry
+        telemetry: false,
+        // Disable web workers
+        useWebWorkers: false,
+    };
 
     return (
-        <VerticalLayout className={'w-full h-full'}>
-          <textarea
-            value={code}
-            onChange={(event) => setCode(event.target.value)}
-            style={{
-                flex: 1,
-                width: '98%',
-                height: '100%',
-                fontFamily: 'monospace',
-                fontSize: '16px',
-                padding: '10px',
-                border: '1px solid #ccc',
-                borderRadius: '5px',
-                outline: 'none',
-                resize: 'none',
-                overflow: "auto",
-                backgroundColor: '#1e1e1e', // VS Code dark theme background color
-                color: '#d4d4d4',
-              }}
-          />
-        </VerticalLayout>
+        <MonacoEditor
+            width="800"
+            height="600"
+            language="javascript"
+            theme="vs-dark"
+            options={options}
+        />
     );
 };
+
 export default CodeEditor;
